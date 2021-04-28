7f523c5337b3629b6383291f966d632971988af5
Roma <nolifeonquiet@gmail.com>
2021-04-29 03:20:39 +0700

    ver. 1.1

---
+++ source/assets/js/app.js
@@ -0,0 +1,18 @@
+//Это функционал споллера (Не полная а на этапе открыть закрыть)
+/* const spolers = document.querySelectorAll('.click');
+
+if (spolers) {
+    for (let i = 0;i < spolers.length; i++) {
+        const spoler = spolers[i];
+
+        spoler.addEventListener('click', function (e) {
+            let href = spoler.getAttribute('href').replace('#', '');
+        
+            const curentModal = document.getElementById(href);
+
+            curentModal.classList.toggle('_active');
+            
+            e.preventDefault();
+        });
+    }
+} */
