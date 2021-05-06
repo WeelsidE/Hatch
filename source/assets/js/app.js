1ea98140c11247df708278ba90b9a871713ea9af
Roma <nolifeonquiet@gmail.com>
2021-05-06 16:55:32 +0700

    ver. 1.3

--- source/assets/js/app.js
+++ source/assets/js/app.js
@@ -7,12 +7,14 @@ $(document).ready(function () {
         }
 
         $(this).toggleClass('_show').next().slideToggle(600);
-
-
     });
 });
 
-
+if ($('.menu__section_header').hasClass('_show') == true) {
+    $('.menu__section_body').addClass('_active');
+} else {
+    $('.menu__section_body').removeClass('_active');
+}
 
 
 
@@ -27,7 +29,12 @@ if (menuLinks.length > 0) {
         menuLink.addEventListener('click', function (e) {
             let menuLink = e.target;
 
-            //console.log(document.querySelector(document.querySelector(menuLink.dataset.goto)));
+        //Закрытие при активном меню
+            document.querySelector('.menu__body').classList.remove('_active');
+            document.querySelector('.menu').classList.remove('_active');
+            menuIcon.classList.remove('_active');
+
+            document.querySelector('body').classList.remove('no-scroll');
 
             if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
                 const gotoBlock = document.querySelector(menuLink.dataset.goto);
@@ -56,40 +63,59 @@ if (menuLinks.length > 0) {
 
 //Сдайдер
 $('.slider__galery').slick({
-    slidesToShow:5,
+    slidesToShow: 5,
+    slidesToScroll: 1,
     dots: false,
     arrows: false,
     adaptiveHeight: true,
     autoplay: true,
-    centerMode: true
-});
-
-$('.slider__tt').slick({
-    dots: true,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 4,
+    centerMode: true,
     responsive: [
     {
         breakpoint: 1024,
         settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
+            slidesToShow: 3
         }
-    },{
+    },
+    {
         breakpoint: 600,
         settings: {
         slidesToShow: 2,
-        slidesToScroll: 2
         }
-    },{
+    },
+    {
         breakpoint: 480,
         settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToShow: 1
         }
     }]
+});
+
+
+
+/* $(document).ready(function(){
+    $('.slider_').slick({
+        arrows: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: false
+    });
+});
+ */
+
+
+
+
+
+//Бургер
+const menuIcon = document.querySelector('.menu__icon');
+
+menuIcon.addEventListener('click', function (e) {
+    $('.menu__body').toggleClass('_active');
+    $('.menu').toggleClass('_active');
+    $(menuIcon).toggleClass('_active');
+
+    $('body').toggleClass('no-scroll');
+
+    e.preventDefault();
 });
